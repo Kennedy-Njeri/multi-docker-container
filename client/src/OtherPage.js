797e734826a6fa725b7b92337a8f8efd544f14d2
Kennedy Njeri <mistakenz123@gmail.com>
2021-01-05 23:44:21 +0300

    removed git submodule

---
+++ client/src/OtherPage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+
+
+const OtherPage = () => {
+
+    return (
+        <div>
+            Im some other page!
+            <Link to="/"></Link>
+        </div>
+    )
+}
+
+
+export default OtherPage
